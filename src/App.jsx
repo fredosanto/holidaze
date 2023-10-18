c02d68b9aae34cce9633229fd71f8ac42566d932
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-18 16:50:52 +0200

    test: font styles and colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,19 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Holidaze</h1>
+      <nav className="text-xl font-light flex bg-blue hover:bg-blueHover">
+        <div id="logo" className="text-4xl font-bold">
+          Holidaze
+        </div>
+        <div>Home</div>
+        <div>Venues</div>
+        <div>Profile</div>
+      </nav>
+      <h1 className="text-4xl font-extrabold">Holidaze</h1>
+      <h2 className="text-2xl font-bold">Hi im the h2</h2>
+      <h3 className="text-lg font-bold">This is the h3</h3>
+      <p className="text-base font-normal">This is the paragraph</p>
+      <p className="text-base font-medium">This is the medium paragrah</p>
     </>
   );
 }
