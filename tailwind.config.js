4362a940e59f19b32b419b9c03ce9c0fc068865b
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-10-18 16:50:28 +0200

    add: custom color styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      blue: "#BABEF7",
+      blueHover: "#969CF0",
+      green: "#C4EBBC",
+      greenHover: "#A8E19B",
+      red: "#F8C9CC",
+      light: "#F7F5F3",
+      disabled: "#EEEEEE",
+    },
     extend: {},
   },
   plugins: [],
