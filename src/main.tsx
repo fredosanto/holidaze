c8c20ac27d313538a5085e13291d303cc68bf2f6
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-11-12 17:18:55 +0100

    fix: ts error

--- src/main.tsx
+++ src/main.tsx
@@ -24,8 +24,7 @@ import "./index.css";
 //     </BrowserRouter>
 //   </React.StrictMode>
 // );
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
