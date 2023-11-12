622f4239d93a99e6541e3b9547505e431d04dd5d
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-11-12 17:19:53 +0100

    feat: ProfileVenue

---
+++ src/components/cards/ProfileVenue.tsx
@@ -0,0 +1,16 @@
+function ProfileVenue() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <h3>Venue name</h3>
+      <p>Location</p>
+      <p>Booking total</p>
+      <p>Venue price tota</p>
+      <p>Venue price,- per night</p>
+    </div>
+  );
+}
+
+export default ProfileVenue;
