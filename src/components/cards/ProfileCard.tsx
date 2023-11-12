374fe9d7316298d58e06561b50ec26f2a519b0b4
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-11-12 17:19:45 +0100

    feat:  profileCard

---
+++ src/components/cards/ProfileCard.tsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+import useLogout from "../../api/auth/useLogout";
+import { User } from "../../types/user";
+
+function ProfileCard({
+  avatar,
+  name,
+  email,
+  venueManager,
+}: Omit<User, "password">) {
+  return (
+    <div className="container flex justify-center w-1/2 m-auto bg-light py-5 rounded-lg text-center">
+      <div>
+        <img
+          src={avatar}
+          alt=""
+          className=" object-cover h-32 w-32 rounded-full m-auto"
+        />
+        <Link to="/avatar" className="underline">
+          Change avatar
+        </Link>
+        <div className="my-5">
+          <h2>{name}</h2>
+          <p>{email}</p>
+          <p>Manager access: {venueManager ? "Yes" : "No"}</p>
+        </div>
+        <button
+          onClick={useLogout}
+          className="bg-blue hover:bg-blueHover py-2 px-6 rounded-md hover:transition-all ease-in hover:duration-300 duration-150 hover:rounded-xl"
+        >
+          Logout
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
