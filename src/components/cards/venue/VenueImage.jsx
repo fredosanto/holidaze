054ce1a3d1af569561c9478abdd5410ba0c16cc0
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-11-14 21:47:38 +0100

    adjust: changes to components

--- src/components/cards/venue/VenueImage.jsx
+++ src/components/cards/venue/VenueImage.jsx
@@ -1,8 +1,8 @@
 function VenueImage({ image, name, maxGuests }) {
   return (
-    <div className="relative overflow-hidden rounded-xl w-1/2 h-72 bg-disabled">
+    <div className="relative overflow-hidden rounded-xl  ">
       {image ? (
-        <img src={image} alt={name} className="object-cover" />
+        <img src={image} alt={name} className="object-cover w-full  h-72" />
       ) : (
         <div className="absolute left-1/2 top-1/2">Image N/A</div>
       )}
