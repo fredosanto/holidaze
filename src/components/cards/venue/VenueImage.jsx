a34d31dfe604b91f255f1a854d50dd9fce16556e
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-11-19 18:19:53 +0100

    adj: tweeks on props and styling

--- src/components/cards/venue/VenueImage.jsx
+++ src/components/cards/venue/VenueImage.jsx
@@ -2,13 +2,13 @@ function VenueImage({ image, name, maxGuests }) {
   return (
     <div className="relative overflow-hidden rounded-xl  ">
       {image ? (
-        <img src={image} alt={name} className="object-cover w-full  h-72" />
+        <img src={image[0]} alt={name} className="object-cover w-full  h-72" />
       ) : (
-        <div className="absolute left-1/2 top-1/2">Image N/A</div>
+        <div className="absolute left-10 top-1/2">Image N/A</div>
       )}
 
       {maxGuests && (
-        <div className="absolute top-0 left-0">
+        <div className="absolute top-0 left-0 py-3 px-5 bg-black text-white bg-opacity-70 rounded-br-xl font-bold text-center">
           <p>Guests:</p>
           <p>{maxGuests}</p>
         </div>
