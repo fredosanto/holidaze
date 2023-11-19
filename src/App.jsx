78b0970b822d6583d97b94cc596262262d6eaaa2
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-11-19 18:24:08 +0100

    delete: unused code for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
 import { Route, Routes } from "react-router";
-import {
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Home from "./routes/Home";
 import ErrorPage from "./routes/ErrorPage";
@@ -16,29 +11,6 @@ import Add from "./routes/Add";
 import { Manage } from "./routes/Manage";
 import { UpdateVenue } from "./routes/UpdateVenue";
 import "./App.css";
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Routes>
-//       {/* <Route path="/" element={<Layout />} errorElement={<ErrorPage />}> */}
-//       <Route path="/" element={<Layout />}>
-//         <Route index element={<Home />} />
-//         <Route path="/venues" element={<Venues />} />
-//         <Route path="/profile" element={<Profile />} />
-//         <Route path="/login" element={<Login />} />
-//         <Route path="/register" element={<Register />} />
-//         <Route path="/avatar" element={<Avatar />} />
-//         <Route path="/add" element={<Add />} />
-//         <Route path="/manage" element={<Manage />} />
-//         <Route path="*" element={<ErrorPage />} />
-//       </Route>
-//     </Routes>
-//   )
-// );
-
-// function App() {
-//   return <RouterProvider router={router} />;
-// }
 
 function App() {
   return (
