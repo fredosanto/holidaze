21f8d86ed5598e8397106513393d8a89f6428e1f
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-11-29 15:55:19 +0100

    add: using reused components

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,24 +1,20 @@
-// import { HeaderImage } from "../components/layout/components/HeaderImage";
+import { Link } from "react-router-dom";
+import { HeaderImage } from "../components/layout/components/HeaderImage";
+import headerBackground from "../assets/images/patrick-robert-doyle-AH8zKXqFITA-unsplash.jpg";
+import { FlexCard } from "../components/layout/components/FlexCard";
+
 function Home() {
-  const image =
-    "https://images.unsplash.com/photo-1596394516093-501ba68a0ba6?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const headerContent =
+    "Welcome to Holidaze! Your next favourite venue is waiting.";
   return (
     <div>
-      <header
-        className="flex h-screen bg-cover bg-no-repeat bg p-5"
-        style={{ backgroundImage: `url(${image})` }}
-      >
-        <div className="m-auto bg-black p-10 bg-opacity-80 rounded-lg">
-          <h1 className=" text-white bg-opacity-100 text-center w-auto text-xl max-w-lg">
-            Welcome to Holidaze! Your next favourite venue is waiting.
-          </h1>
-        </div>
-      </header>
-
-      <h2 className="text-2xl font-bold">Popular venues</h2>
-      <h3 className="text-lg font-bold">This is the h3</h3>
-      <p className="text-base font-normal">This is the paragraph</p>
-      <p className="text-base font-medium">This is the medium paragrah</p>
+      <HeaderImage image={headerBackground} content={headerContent} />
+      <div className="max-w-4xl my-12 mx-auto bg-green py-10 md:px-5 md:rounded-lg">
+        <FlexCard revert={false} />
+      </div>
+      <div className="max-w-4xl my-12 mx-auto bg-light py-10 md:px-5 md:rounded-lg">
+        <FlexCard revert={true} />
+      </div>
     </div>
   );
 }
--- src/routes/Venues.jsx
+++ src/routes/Venues.jsx
@@ -5,15 +5,16 @@ import { useFetch } from "../hooks/useFetch";
 import { API } from "../api/enpoints";
 import { Typeahead } from "../components/Typeahead";
 import { useState } from "react";
+import headerBackgound from "../assets/images/roberto-nickson-emqnSQwQQDo-unsplash.jpg";
+import { HeaderImage } from "../components/layout/components/HeaderImage";
 
 function Venues() {
+  const headerContent =
+    "We have the perfect venue for everyone! Book a venue today!";
   const [pagination, setPagination] = useState(10);
   const urlParameters = `?sort=created&sortOrder=desc&limit=${pagination}&_owner=true&_bookings=true`;
   const url = `${API.venues.$ + urlParameters}`;
   const { data: venues, isLoading, error } = useFetch(url);
-
-  const image =
-    "https://images.unsplash.com/photo-1596394516093-501ba68a0ba6?q=80&w=2970&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   function handlePagination() {
     setPagination(pagination + 10);
@@ -30,16 +31,7 @@ function Venues() {
 
   return (
     <>
-      <header
-        className="flex h-screen bg-cover bg-no-repeat bg p-5"
-        style={{ backgroundImage: `url(${image})` }}
-      >
-        <div className="m-auto bg-black p-10 bg-opacity-80 rounded-lg">
-          <h1 className=" text-white bg-opacity-100 text-center w-auto text-xl max-w-lg">
-            We have the perfect venue for everyone! Book a venue today!
-          </h1>
-        </div>
-      </header>
+      <HeaderImage image={headerBackgound} content={headerContent} />
       <div className="my-10 m-auto max-w-7xl">
         <div className="bg-grey p-5 py-10 my-10 m-auto md:rounded-lg w-full sm:max-w-xl">
           <h2 className="text-center mb-5">Search for venue</h2>
