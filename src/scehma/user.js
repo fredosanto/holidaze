7cdb1c7901afd9b5536d2e840c74e236a2852257
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-12-01 22:16:36 +0100

    add: another validation on user scheme

--- src/scehma/user.js
+++ src/scehma/user.js
@@ -1,7 +1,10 @@
 import * as yup from "yup";
 
 export const userSchema = yup.object().shape({
-  name: yup.string().required("Required field"),
+  name: yup
+    .string()
+    .trim("Name can only use a-Z, 0-9, and _")
+    .required("Required field"),
   email: yup.string().required("Required field"),
   password: yup.string().min(8).required("Required field"),
   avatar: yup.string().optional(),
