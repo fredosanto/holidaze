9b6b23c10febb89683d43000760fbe210b030803
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-12-01 14:22:39 +0100

    bugfix: return if url includes a undefine

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,13 +1,19 @@
 import { useEffect, useState } from "react";
+import { API } from "../api/enpoints";
 
 export function useFetch(url) {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  const errorUrl = `${API.venues.id(undefined).$}?_owner=true&_bookings=true`;
+
   useEffect(() => {
     async function fetchData() {
       try {
+        if (url === errorUrl) {
+          return;
+        }
         const response = await fetch(url);
 
         if (!response.ok) {
