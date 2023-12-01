012b8b8ebb152331e7d2310c8c6ad9eddfdaacd7
fredosanto <93183340+fredosanto@users.noreply.github.com>
2023-12-01 22:16:51 +0100

    del: unused code

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -19,11 +19,6 @@ export function Login() {
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
 
-  // const onSubmit = (data) => {
-  //   console.log(data);
-  //   login(url, data);
-  // };
-
   return (
     <div className="h-screen  bg-light">
       <div className="flex flex-col items-center gap-5">
@@ -66,63 +61,3 @@ export function Login() {
     </div>
   );
 }
-
-// function Login() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-//     e.preventDefault();
-//     const loginData: LoginData = {
-//       email,
-//       password,
-//     };
-
-//     useLogin(url, loginData);
-//   }
-
-//   function onInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-//     const value = e.target.value;
-//     if (e.target.name === "email") {
-//       setEmail(value);
-//     }
-//     if (e.target.name === "password") {
-//       setPassword(value);
-//     }
-//   }
-
-//   return (
-//     <div className="h-screen">
-//       <form onSubmit={handleSubmit}>
-//         <div className="loginInput">
-//           <label htmlFor="email">email</label>
-//           <input
-//             type="email"
-//             name="email"
-//             id="email"
-//             value={email}
-//             onChange={onInputChange}
-//             className="border block"
-//           />
-//         </div>
-//         <div className="loginInput">
-//           <label htmlFor="password">password</label>
-//           <input
-//             type="password"
-//             name="password"
-//             id="password"
-//             value={password}
-//             onChange={onInputChange}
-//             className="border block"
-//           />
-//         </div>
-//         <button className="bg-blue hover:bg-blueHover py-2 px-6 rounded-md hover:transition-all ease-in hover:duration-300 duration-150 hover:rounded-xl">
-//           Login
-//         </button>
-//       </form>
-//       <Link to="/register" className="underline">
-//         Register
-//       </Link>
-//     </div>
-//   );
-// }
